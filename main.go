073d33c78f83998491ced89195d3abb4a13a44b9
jerrrren <e0684530@u.nus.edu>
2022-05-30 14:41:53 +0800

    undo first cors

--- main.go
+++ main.go
@@ -14,17 +14,6 @@ var db *sql.DB
 func main() {
 	db = setupDatabase()
 	router := gin.Default()
-	router.Use(cors.New(cors.Config{
-    AllowOrigins:     []string{"https://intronus-frontend.herokuapp.com/"},
-    AllowMethods:     []string{"PUT", "PATCH","GET","POST"},
-    AllowHeaders:     []string{"Origin"},
-    ExposeHeaders:    []string{"Content-Length"},
-    AllowCredentials: true,
-    AllowOriginFunc: func(origin string) bool {
-      return origin == "https://github.com"
-    },
-    MaxAge: 12 * time.Hour,
-    }))
 	AuthRoutes(router)
 	UserRoutes(router)
 	router.Run()
