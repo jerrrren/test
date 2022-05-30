feefddcf67192dc5cb3c199711b02c6e13358417
jerrrren <e0684530@u.nus.edu>
2022-05-30 11:37:03 +0800

    testing for heroku

--- db.go
+++ db.go
@@ -3,19 +3,21 @@ package main
 import (
 	"database/sql"
 	"fmt"
+
+	_ "github.com/lib/pq"
 )
 
 const (
-	host     = "localhost"
-	port     =  5400
-	user     = "postgres"
-	password = "docker"
-	dbname   = "postgres"
+	host     = "ec2-54-211-255-161.compute-1.amazonaws.com"
+	port     =  5432
+	user     = "fdikfgagiyyaxq"
+	password = "e6398263fc3d8545d90cb455bc46cbcf7c47fd74ef9e6a8633e15c48c90feeff"
+	dbname   = "db2jrt4m1j59dh"
 )
 
 func setupDatabase() *sql.DB {
 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
-		"password=%s dbname=%s sslmode=disable",
+		"password=%s dbname=%s sslmode=require",
 		host, port, user, password, dbname)
 
 	db, err := sql.Open("postgres", psqlInfo)
