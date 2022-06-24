9b02561e1799c2255e6068cafc2baa6c598a9013
jerrrren <cheejeren@yahoo.com.sg>
2022-06-24 22:02:48 +0800

    changes

--- db/db.go
+++ db/db.go
@@ -4,15 +4,15 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
-	"os"
-	"strconv"
+//	"os"
+//	"strconv"
 
 	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 )
 
-/*
-online postgres server
+
+//online postgres server
 const (
 	host     = "ec2-54-211-255-161.compute-1.amazonaws.com"
 	port     =  5432
@@ -20,7 +20,7 @@ const (
 	password = "e6398263fc3d8545d90cb455bc46cbcf7c47fd74ef9e6a8633e15c48c90feeff"
 	dbname   = "db2jrt4m1j59dh"
 )
-*/
+
 
 var DB *sql.DB = setupDatabase()
 
@@ -30,7 +30,7 @@ func setupDatabase() *sql.DB {
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
-
+/*
 	var (
 		host     = os.Getenv("HOST")
 		port, _  = strconv.Atoi(os.Getenv("DB_PORT"))
@@ -38,7 +38,7 @@ func setupDatabase() *sql.DB {
 		password = os.Getenv("PASSWORD")
 		dbname   = os.Getenv("DB")
 	)
-
+*/
 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
 		"password=%s dbname=%s sslmode=disable", //need to change when uploading
 		host, port, user, password, dbname)
