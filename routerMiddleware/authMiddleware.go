41c58b9ba823d04c98592cdb619137082390d7ce
jerrrren <cheejeren@yahoo.com.sg>
2022-06-24 23:35:49 +0800

    changes

--- routerMiddleware/authMiddleware.go
+++ routerMiddleware/authMiddleware.go
@@ -33,7 +33,7 @@ func Authenticate() gin.HandlerFunc{
 func CORSMiddleware() gin.HandlerFunc {
     return func(c *gin.Context) {
         c.Writer.Header().Set("Access-Control-Allow-Origin", "https://intronusfrontend.herokuapp.com")
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+		//c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
         c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
         c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
         c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
